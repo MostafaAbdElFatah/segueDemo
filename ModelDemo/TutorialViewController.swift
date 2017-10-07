8d61c7cfccadb9664e19c0c2ef5f2bb30d000110
Mostafa <MostafaAbdEl_Fatah@192.168.179.128>
2017-10-07 10:04:42 -0700

    segue demo

---
+++ ModelDemo/TutorialViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TutorialViewController.swift
+//  ModelDemo
+//
+//  Created by Mostafa on 7/16/17.
+//  Copyright © 2017 Mostafa. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func dismiss(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
